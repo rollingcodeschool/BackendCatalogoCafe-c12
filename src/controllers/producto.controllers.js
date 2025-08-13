0a91bc6daedf43d7731589e6feb18f7e6134fa37
Emilse Arias <emilsearias@gmail.com>
2025-08-13 10:47:44 -0300

    agregamos la logica del controlador crearproducto

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -1,19 +1,21 @@
+import Producto from "../models/producto.js";
 
-export const prueba = (req, res)=>{
-    res.status(201);
-    res.send('Este es un mensaje desde el controlador')
-}
+export const prueba = (req, res) => {
+  res.status(201);
+  res.send("Este es un mensaje desde el controlador");
+};
 
-export const crearProducto = async (req, res)=>{
-    try{
-        // console.log(req)
-        console.log(req.body)
-        //1- validar los datos del req.body
-        //2- crear el producto en la base de datos
-        //3- enviar el mensaje de respuesta
-        res.send('desde la logica de crear producto')
-    }catch(error){
-        console.error(error)
-    }
-
-}
+export const crearProducto = async (req, res) => {
+  try {
+    // console.log(req)
+    //1- validar los datos del req.body
+    //2- crear el producto en la base de datos
+    const productoNuevo = new Producto(req.body);
+    await productoNuevo.save();
+    //3- enviar el mensaje de respuesta
+    res.status(201).json({ mensaje: "El producto fue creado correctamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al crear el producto"});
+  }
+};
