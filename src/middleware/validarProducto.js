d6af39b4d9d2bf2e271a64d0eb30ab3848d6adc5
Emilse Arias <emilsearias@gmail.com>
2025-08-25 11:48:05 -0300

    agrgeamos la validacion de la categoria del producto

--- src/middleware/validarProducto.js
+++ src/middleware/validarProducto.js
@@ -7,6 +7,7 @@ const validacionProducto = [
     .withMessage("El nombre del producto es obligatorio")
     .isLength({ min: 2, max: 100 })
     .withMessage("El nombre del producto debe tener entre 2 y 100 caracteres"),
+    // todo: agregar una validacion customizada para verificar si el producto existe
   body("precio")
     .notEmpty()
     .withMessage("El precio es obligatorio")
@@ -19,6 +20,11 @@ const validacionProducto = [
         throw new Error("El precio debe estar entre 50 y 1000000");
       }
     }),
+    body("categoria")
+    .notEmpty()
+    .withMessage("La categoría es obligatoria")
+    .isIn(["Infusiones", "Batidos", "Dulce", "Salado"])
+    .withMessage("La categoría no es válida, deberia ser una de las siguientes opciones: Infusiones, Batidos, Dulce, Salado"),
   (req, res, next) => resultadoValidacion(req, res, next),
 ];
 
