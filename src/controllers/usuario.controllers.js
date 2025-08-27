a020c2dee130556c2ad47876bc382d167fc1e826
Emilse Arias <emilsearias@gmail.com>
2025-08-27 10:01:39 -0300

    agregamos la logica para crear un usuario

--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -2,7 +2,9 @@ import Usuario from "../models/usuario.js";
 
 export const crearUsuario = async (req,res)=>{
     try {
-        res.send('Desde la funcion crearusuario')
+        const usuarioNuevo = new Usuario(req.body)
+        await usuarioNuevo.save()
+        res.status(201).json({mensaje: 'El usuario fue creado correctamente'});
     } catch (error) {
         console.error(error)
         res.status(500).json({error: "Error al crear el usuario"})
