6f4d8f34e4c8b76babe49045c25598d95987a70c
Emilse Arias <emilsearias@gmail.com>
2025-09-24 13:20:18 -0300

    agregamos las rutas y controlador para generar una preferencia de pago con mercadoPago

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import productoRouter from "./producto.routes.js";
 import usuarioRouter from "./usuario.routes.js";
-
+import pagosRouter from "./pagos.routes.js"
 const router = Router();
 
 router.use('/productos', productoRouter)
 router.use('/usuarios', usuarioRouter)
+router.use('/pagos', pagosRouter)
 
 export default router;
